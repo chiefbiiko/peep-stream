a8d55e09f166a64270ec6a2d247c5942abe712dc
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-05-18 22:33:49 +0200

    simplify

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-var { Transform } = require('stream')
+var Transform = require('stream').Transform
 
-function Peep (opts) {
-  if (!(this instanceof Peep)) return new Peep(opts)
+var BUF419 = Buffer.from([ 0, 4, 1, 9, 4, 1, 9, 0 ])
+
+function Peep (dump, opts) {
+  if (!(this instanceof Peep)) return new Peep(dump, opts)
   Transform.call(this)
-  this._seq = 0
-  this._stash = []
+  this._delimiter = opts.delimiter || BUF419
+  if (global) {
+    dump = dump || new Date().toISOString() + '.peep'
+    this._dump = require('fs').createWriteStream(dump)
+  } else if (window) {
+    this._dump = require('websocket-stream')('ws://localhost:41900')
+  } else {
+    throw new Error('unknown context')
+  }
 }
 
 inherits(Peep, Transform)
 
 Peep.prototype._transform = function transform (chunk, _, next) {
-  this._stash.unshift({ seq: this._seq++, chunk: chunk })
   this.push(chunk)
-  next()
-}
+  var ts = Buffer.alloc(8)
+  ts.writeDoubleBE(Date.now(), 0)
+  // ... write
 
-Peep.prototype.next = function next () {
-  return this._stash.pop()
+  next()
 }
 
 module.exports = Peep
